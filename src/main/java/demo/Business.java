0e39b2cbf151abcd7a7abecb9af434cb7fe4397f
Dieter Holz <dieter.holz@fhnw.ch>
2021-09-24 10:33:51 +0200

    Initial commit

---
+++ src/main/java/demo/Business.java
@@ -0,0 +1,14 @@
+package demo;
+
+/**
+ * This class implements the business logic of the world domination product.
+ */
+class Business {
+
+    // tag::calculateKeyFigureMethod[]
+    public int calculateKeyFigure(){
+        return 42;
+    }
+    // end::calculateKeyFigureMethod[]
+
+}
