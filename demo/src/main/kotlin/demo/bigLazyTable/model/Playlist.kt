f566f6178ee42c1ef8fff6d2348df1b51e6ee6a0
Marco <marco.sprenger@students.fhnw.ch>
2021-10-28 13:44:46 +0200

    Changed Playlist.kt to data class with immutable fields, added id field to class

--- demo/src/main/kotlin/demo/bigLazyTable/model/Playlist.kt
+++ demo/src/main/kotlin/demo/bigLazyTable/model/Playlist.kt
@@ -1,29 +1,34 @@
 package demo.bigLazyTable.model
 
-class Playlist(
-    var name: String = "",
-    var collaborative: Boolean = false,
-    var modified_at: String = "",
-    var num_tracks: String = "",
-    var num_albums: String = "",
-    var num_followers: String = "",
-    var num_edits: String = "",
-    var duration_ms: String = "",
-    var num_artists: String = "",
-    var track0_artist_name: String = "",
-    var track0_track_name: String = "",
-    var track0_duration_ms: String = "",
-    var track0_album_name: String = "",
-    var track1_artist_name: String = "",
-    var track1_track_name: String = "",
-    var track1_duration_ms: String = "",
-    var track1_album_name: String = "",
-    var track2_artist_name: String = "",
-    var track2_track_name: String = "",
-    var track2_duration_ms: String = "",
-    var track2_album_name: String = "",
-    var track3_artist_name: String = "",
-    var track3_track_name: String = "",
-    var track3_duration_ms: String = "",
-    var track3_album_name: String = ""
+/**
+ * @author Marco Sprenger
+ * @author Livio Näf
+ */
+data class Playlist(
+    val name: String = "",
+    val collaborative: Boolean = false,
+    val modified_at: String = "",
+    val num_tracks: String = "",
+    val num_albums: String = "",
+    val num_followers: String = "",
+    val num_edits: String = "",
+    val duration_ms: String = "",
+    val num_artists: String = "",
+    val track0_artist_name: String = "",
+    val track0_track_name: String = "",
+    val track0_duration_ms: String = "",
+    val track0_album_name: String = "",
+    val track1_artist_name: String = "",
+    val track1_track_name: String = "",
+    val track1_duration_ms: String = "",
+    val track1_album_name: String = "",
+    val track2_artist_name: String = "",
+    val track2_track_name: String = "",
+    val track2_duration_ms: String = "",
+    val track2_album_name: String = "",
+    val track3_artist_name: String = "",
+    val track3_track_name: String = "",
+    val track3_duration_ms: String = "",
+    val track3_album_name: String = "",
+    val id: Int = 0
 )
