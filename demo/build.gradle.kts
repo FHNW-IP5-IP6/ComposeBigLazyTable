3eb8ec1532649db88c68e97a8f8a15c11a1e29f4
Marco <marco.sprenger@students.fhnw.ch>
2021-11-11 12:32:42 +0100

    changed sonarqube settings

--- demo/build.gradle.kts
+++ demo/build.gradle.kts
@@ -8,12 +8,12 @@ plugins {
     id("org.sonarqube") version "3.3"
 }
 
-//sonarqube {
-//    properties {
-//        property("sonar.projectKey", "demo")
-//        property("sonar.login", "e11521286d382bb374dbf68bc8642d4486e24ee3")
-//    }
-//}
+// sonarqube configuration
+sonarqube {
+    properties {
+        property("sonar.projectKey", "demo")
+    }
+}
 
 group = "ch.fhnw"
 version = "1.0"
