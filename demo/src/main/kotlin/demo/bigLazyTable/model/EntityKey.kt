708da85c2d286086b50136d253620144f981d8e5
Marco <marco.sprenger@students.fhnw.ch>
2021-11-29 15:01:43 +0100

    pair programming session

---
+++ demo/src/main/kotlin/demo/bigLazyTable/model/EntityKey.kt
@@ -0,0 +1,24 @@
+package demo.bigLazyTable.model
+
+import java.util.*
+
+class EntityKey(private val type: String, private val id: Long) {
+    fun getType() = type
+
+    fun getId() = id
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) {
+            return true
+        }
+        if (other == null || this.javaClass != other.javaClass) {
+            return false
+        }
+        val entityKey = other as EntityKey
+        return id == entityKey.id && Objects.equals(type, entityKey.type)
+    }
+
+    override fun hashCode(): Int = Objects.hash(type, id)
+
+    override fun toString(): String = "Entity{type='$type\', id=$id}"
+}
