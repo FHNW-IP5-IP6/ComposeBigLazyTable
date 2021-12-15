e46151150102aad6c3185b5ec6aa3e9fbcb541a7
Livio Naef <livio.naef@students.fhnw.ch>
2021-12-15 21:26:24 +0100

    addded describing method names intead of comments

--- demo/src/main/kotlin/demo/bigLazyTable/BigLazyTableApp.kt
+++ demo/src/main/kotlin/demo/bigLazyTable/BigLazyTableApp.kt
@@ -2,13 +2,14 @@ package demo.bigLazyTable
 
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
+import androidx.compose.ui.window.FrameWindowScope
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.WindowPlacement
 import androidx.compose.ui.window.application
-import demo.bigLazyTable.data.database.DBService
 import demo.bigLazyTable.model.ViewModelLazyList
-import demo.bigLazyTable.ui.ComposeListsUI
+import demo.bigLazyTable.ui.BigLazyTableUI
 import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.transactions.TransactionManager
 import java.sql.Connection
@@ -23,18 +24,20 @@ fun main() = application {
         onCloseRequest = ::exitApplication,
         title = "ComposeLists"
     ) {
-        // Set window size to Window-Fullscreen
-        window.placement = WindowPlacement.Maximized
-        // Initialize database connection
-        setupDatabase()
-        // Initialize composeForms.model and load first batch of data to display the UI
-        val model = remember { ViewModelLazyList }
-        // Initialize UI
-        ComposeListsUI(model = model)
+        setWindowSizeToFullscreen()
+
+        initializeDatabaseConnection()
+
+        val model = remember { ViewModelLazyList } // side effect: init loads first data to display
+        BigLazyTableUI(model = model)
     }
 }
 
-private fun setupDatabase() {
+private fun FrameWindowScope.setWindowSizeToFullscreen() {
+    window.placement = WindowPlacement.Maximized
+}
+
+private fun initializeDatabaseConnection() {
     Database.connect("jdbc:sqlite:./demo/src/main/resources/spotify_playlist_dataset.db", "org.sqlite.JDBC")
     TransactionManager.manager.defaultIsolationLevel = Connection.TRANSACTION_SERIALIZABLE
 }
