e33604e753dcc282a9f4ceb546d35b0387c46d7f
Livio Naef <livio.naef@students.fhnw.ch>
2021-12-20 19:03:27 +0100

    Moved initialisation of Db to separate class
    that object also holds all the values for url, driver, isolationLevel

--- demo/src/main/kotlin/demo/bigLazyTable/BigLazyTableApp.kt
+++ demo/src/main/kotlin/demo/bigLazyTable/BigLazyTableApp.kt
@@ -2,17 +2,15 @@ package demo.bigLazyTable
 
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.window.FrameWindowScope
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.WindowPlacement
 import androidx.compose.ui.window.application
+import demo.bigLazyTable.data.database.Db
 import demo.bigLazyTable.model.ViewModelLazyList
 import demo.bigLazyTable.ui.BigLazyTableUI
-import org.jetbrains.exposed.sql.Database
-import org.jetbrains.exposed.sql.transactions.TransactionManager
-import java.sql.Connection
+import java.awt.Dimension
 
 /**
  * @author Marco Sprenger, Livio Näf
@@ -26,7 +24,7 @@ fun main() = application {
     ) {
         setWindowSizeToFullscreen()
 
-        initializeDatabaseConnection()
+        Db.initializeConnection()
 
         val model = remember { ViewModelLazyList } // side effect: init loads first data to display
         BigLazyTableUI(model = model)
@@ -34,10 +32,8 @@ fun main() = application {
 }
 
 private fun FrameWindowScope.setWindowSizeToFullscreen() {
-    window.placement = WindowPlacement.Maximized
-}
-
-private fun initializeDatabaseConnection() {
-    Database.connect("jdbc:sqlite:./demo/src/main/resources/spotify_playlist_dataset.db", "org.sqlite.JDBC")
-    TransactionManager.manager.defaultIsolationLevel = Connection.TRANSACTION_SERIALIZABLE
+    window.apply {
+        minimumSize = Dimension(1000, 800)
+        placement = WindowPlacement.Maximized
+    }
 }
