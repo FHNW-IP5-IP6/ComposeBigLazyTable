8969ad54c089d879e4c2b5515037ace3f500fd79
Livio Naef <livio.naef@students.fhnw.ch>
2021-12-23 12:53:40 +0100

    Added Hover & Unhover Color

--- demo/src/main/kotlin/demo/bigLazyTable/ui/theme/Color.kt
+++ demo/src/main/kotlin/demo/bigLazyTable/ui/theme/Color.kt
@@ -1,8 +1,12 @@
 package demo.bigLazyTable.ui.theme
 
+import androidx.compose.ui.graphics.Color
 import composeForms.ui.theme.ColorsUtil.Companion.get
 import composeForms.ui.theme.FormColors
 
 val BackgroundColorHeader = get(FormColors.BACKGROUND_COLOR_HEADER)
 val BackgroundColorGroups = get(FormColors.BACKGROUND_COLOR_GROUPS)
 val BackgroundColorLight = get(FormColors.BACKGROUND_COLOR_LIGHT)
+
+val HoverColor = Color.Red.copy(alpha = 0.8f)
+val UnhoverColor = Color.Red.copy(alpha = 0.3f)
