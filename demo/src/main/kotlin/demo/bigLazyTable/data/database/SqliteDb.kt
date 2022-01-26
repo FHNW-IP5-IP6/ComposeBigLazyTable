2b72105554c1adb89b5aae9f36eb473bd4dea6dc
Livio Naef <livio.naef@students.fhnw.ch>
2022-01-26 19:46:57 +0100

    caseSensitiveFiltering: Boolean or queryParams: List<String>

--- demo/src/main/kotlin/demo/bigLazyTable/data/database/SqliteDb.kt
+++ demo/src/main/kotlin/demo/bigLazyTable/data/database/SqliteDb.kt
@@ -4,6 +4,7 @@ import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.transactions.TransactionManager
 import java.sql.Connection
 
+// TODO: Instead of caseSensitiveFiltering: Boolean a queryParams: List<String> so that multiple query strings can be added and its more dynamic
 class SqliteDb(pathToDb: String, caseSensitiveFiltering: Boolean) {
 
     private val makeSqliteCaseSensitive = "?case_sensitive_like=true"
