759507c2aaf9340086b402dbf65ac43ace783cf5
Marco Sprenger <msprenger99@gmx.ch>
2022-02-04 14:33:43 +0100

    Improved scheduler

--- demo/src/main/kotlin/demo/bigLazyTable/model/Scheduler.kt
+++ demo/src/main/kotlin/demo/bigLazyTable/model/Scheduler.kt
@@ -2,6 +2,7 @@ package demo.bigLazyTable.model
 
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 object Scheduler {
@@ -12,16 +13,21 @@ object Scheduler {
         if (inProcess) return
         if (task == null) return
         inProcess = true
+        val taskToDo = task
         CoroutineScope(Dispatchers.IO).launch {
-            task!!.invoke(null)
+            println("Task in process: ${taskToDo.hashCode()}")
+            delay(2000)
+            taskToDo!!.invoke(null)
         }.invokeOnCompletion {
+            println("Task finished: ${taskToDo.hashCode()}")
             inProcess = false
-            task = null
+            if (task!! == taskToDo) task = null
             process()
         }
     }
 
     fun add(task: (Any?) -> Unit) {
+        println("Task added to scheduler: ${task.hashCode()}")
         this.task = task
         process()
     }
