c799eb2143347fbad3317e5d2856e6ceafa86f60
Livio Naef <livio.naef@students.fhnw.ch>
2022-02-09 17:18:24 +0100

    Recomposition happens after language change and renders all non remember items again
    - removed initializeWindowSize out of Theme where it doesnt belong

--- demo/src/main/kotlin/demo/bigLazyTable/App.kt
+++ demo/src/main/kotlin/demo/bigLazyTable/App.kt
@@ -3,14 +3,16 @@ package demo.bigLazyTable
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.runtime.remember
+import androidx.compose.ui.window.FrameWindowScope
 import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.WindowPlacement
 import androidx.compose.ui.window.application
 import demo.bigLazyTable.data.database.DBService
 import demo.bigLazyTable.data.database.SqliteDb
 import demo.bigLazyTable.model.AppState
 import demo.bigLazyTable.model.LazyTableViewModel
 import demo.bigLazyTable.ui.BigLazyTableUI
-import demo.bigLazyTable.ui.theme.initializeWindowSize
+import java.awt.Dimension
 
 /**
  * @author Marco Sprenger, Livio Näf
@@ -22,18 +24,25 @@ fun main() = application {
         onCloseRequest = ::exitApplication,
         title = "ComposeLists"
     ) {
-        initializeWindowSize()
+        // Needs remember. Without it, the first language change in no full-screen leads to a full-screen window
+        remember { initializeWindowSize() }
 
-        SqliteDb(
-            pathToDb = "./demo/src/main/resources/spotify_playlist_dataset.db",
-            caseSensitiveFiltering = true
-        ).initializeConnection()
+        // Needs remember. Without it, initializeConnection would be called again (f.e. on language change)
+        remember {
+            SqliteDb(
+                pathToDb = "./demo/src/main/resources/spotify_playlist_dataset.db",
+                caseSensitiveFiltering = true
+            ).initializeConnection()
+        }
 
         val service = DBService
-        val appState = AppState(pagingService = service)
+
+        // Needs remember. Without it, the view just shows empty (...) Items (happens after language change)
+        val appState = remember { AppState(pagingService = service) }
+
         val viewModel = remember {
             LazyTableViewModel(
-                pagingService = DBService,
+                pagingService = service,
                 appState = appState
             ) // side effect: init loads first data to display
         }
@@ -41,5 +50,12 @@ fun main() = application {
             viewModel = viewModel,
             appState = appState
         )
+    }
+}
+
+fun FrameWindowScope.initializeWindowSize() {
+    window.apply {
+        minimumSize = Dimension(1000, 800)
+        placement = WindowPlacement.Maximized
     }
 }
