b62490e3ff7b7a60afd0c7ce8b65d6c061a5fc61
Marco Sprenger <msprenger99@gmx.ch>
2022-02-10 14:17:24 +0100

    Improved scheduler

--- demo/src/main/kotlin/demo/bigLazyTable/model/Scheduler.kt
+++ demo/src/main/kotlin/demo/bigLazyTable/model/Scheduler.kt
@@ -2,33 +2,37 @@ package demo.bigLazyTable.model
 
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 /**
  * @author Marco Sprenger, Livio Näf
+ *
+ * Scheduler to run only one task. If a new task is added, the old one is overwritten.
+ * If the scheduler is in process, after a short delay the newest task is executes.
+ * After execution the scheduler checks if a new task is available, if not the scheduler is paused.
  */
 object Scheduler {
     private var inProcess = false
-    private var task: ((Any?) -> Unit)? = null
+    private var task: (() -> Unit)? = null
+    private var taskToDo: (() -> Unit)? = null
 
     private fun process() {
         if (inProcess) return
         if (task == null) return
         inProcess = true
-        val taskToDo = task
         CoroutineScope(Dispatchers.IO).launch {
-            //println("Task in process: ${taskToDo.hashCode()}")
-            taskToDo!!.invoke(null)
+            delay(50)
+            taskToDo = task
+            taskToDo!!.invoke()
         }.invokeOnCompletion {
-            //println("Task finished: ${taskToDo.hashCode()}")
             inProcess = false
             if (task!! == taskToDo) task = null
             process()
         }
     }
 
-    fun add(task: (Any?) -> Unit) {
-        //println("Task added to scheduler: ${task.hashCode()}")
+    fun add(task: () -> Unit) {
         this.task = task
         process()
     }
