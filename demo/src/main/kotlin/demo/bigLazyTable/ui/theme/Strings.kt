af82dd910c17ee9cb14a934dcc778abf92873c33
Livio Naef <livio.naef@students.fhnw.ch>
2022-03-21 11:48:51 +0100

    Update Strings.kt

--- demo/src/main/kotlin/demo/bigLazyTable/ui/theme/Strings.kt
+++ demo/src/main/kotlin/demo/bigLazyTable/ui/theme/Strings.kt
@@ -1,5 +1,7 @@
 package demo.bigLazyTable.ui.theme
 
+// TODO: Find a Kotlin solution to internationalisation Problem
+//  that these hardcoded values are updated for multiple languages
 const val FilterLabel = "Filter"
 const val ContentDescriptionSortIcon = "Sort"
 const val ContentDescriptionClearFilterIcon = "Clear Filter"
