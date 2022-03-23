80e03a331d1e72d54eb29d3db7dc9ecfd8544904
Livio Naef <livio.naef@students.fhnw.ch>
2022-03-23 20:03:16 +0100

    Added TODO about dark/ligh color theming

--- common/src/main/kotlin/bigLazyTable/view/theme/Theme.kt
+++ common/src/main/kotlin/bigLazyTable/view/theme/Theme.kt
@@ -1,14 +1,17 @@
 package bigLazyTable.view.theme
 
 import androidx.compose.foundation.ScrollbarStyle
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import composeForms.ui.theme.*
 
+// TODO: Dark/Light Switch works for TextFields -> what colors should we use?
 private val DarkColorPalette = darkColors(
     primary = BackgroundColorGroups,
     primaryVariant = BackgroundColorGroups,
@@ -32,14 +35,13 @@ val CustomScrollbarStyle = ScrollbarStyle(
 
 @Composable
 fun BigLazyTableTheme(
-    //darkTheme: Boolean = isSystemInDarkTheme(),
-    darkTheme: Boolean = true,
+    darkTheme: Boolean = isSystemInDarkTheme(),
     content: @Composable () -> Unit
 ) {
     val colors = if (darkTheme) DarkColorPalette else LightColorPalette
 
     MaterialTheme(
-        //        colors = colors,
+//        colors = colors, TODO: Could be added if the defined colors look good
         content = content
     )
 }
